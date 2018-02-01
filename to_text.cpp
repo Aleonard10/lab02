172c2508071e21c778fed0a9a3d73654b6b4fcc9
Aleonard10 <31227161+Aleonard10@users.noreply.github.com>
2018-02-01 14:34:04 -0800

    Update to_text.cpp

--- to_text.cpp
+++ to_text.cpp
@@ -1,52 +1,52 @@
 #include <iostream>
-#include <stdio>
+#include <stdio.h>
 
 using namespace std;
 
-int main()
+int main ()
 {
-    int number;
+    char numbers;
     int i = 0;
-    
-    while (i < 1)
+    do
     {
-        cin >> number;
-        switch (number){
-        
-        case 0:
-            cout << "zero" << endl;
-            break;
-        case 1:
-            cout << "one" << endl;
-            break;
-        case 2:
-            cout << "two" << endl;
-            break;
-        case 3:
-            cout << "three" << endl;
-            break;
-        case 4:
-            cout << "four" << endl;
-            break;
-        case 5:
-            cout << "five" << endl;
-            break;
-        case 6:
-            cout << "six" << endl;
-            break;
-        case 7:
-            cout << "seven" << endl;
-            break;
-        case 8:
-            cout << "eight" << endl;
-            break;
-        case 9:
-            cout << "nine" << endl;
-            break;
-        default:
-            i++;
-            break;
+        numbers = getchar();
+        switch (numbers)
+        {
+            case '0':
+                cout << "zero" << endl;
+                break;
+            case '1':
+                cout << "one" << endl;
+                break;
+            case '2':
+                cout << "two" << endl;
+                break;
+            case '3':
+                cout << "three" << endl;
+                break;
+            case '4':
+                cout << "four" << endl;
+                break;
+            case '5':
+                cout << "five" << endl;
+                break;
+            case '6':
+                cout << "six" << endl;
+                break;
+            case '7':
+                cout << "seven" << endl;
+                break;
+            case '8':
+                cout << "eight" << endl;
+                break;
+            case '9':
+                cout << "nine" << endl;
+                break;
+            default:
+                break;
+
         }
     }
-    
+    while (getchar() != EOF);
+    return 0;
 }
